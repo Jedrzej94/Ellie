4fe8d3bb4ebca2c1c12f1b4f34a9381b763ad7ac
Jędrzej <Jedrzej94@users.noreply.github.com>
2020-04-23 23:39:30 +0200

    Update Ellie's ping answers list

--- EllieBot/modules/ping.cs
+++ EllieBot/modules/ping.cs
@@ -15,14 +15,18 @@ namespace EllieBot.modules
             Random random = new Random();
             string[] randomMsgs = new string[] 
 				{
-					"I'm gonna kill, every, one, last of them",
+                    "I'm gonna find and I'm gonna kill every last one of them",
 					"Wanna hear a joke?",
 					"What you need?",
 					"Heeyyy-aaaa!",
 					"Happy Christmas!",
-					"It doesn't matter how much you push the envelope. It'll still be stationary.",
-					"I walked into my sister's room and tripped on a bra. It was a booby-trap."
-				};
+                    "I tried to catch some fog earlier. I mist.",
+                    "It doesn't matter how much you push the envelope. It'll still be stationary.",
+					"I walked into my sister's room and tripped on a bra. It was a booby-trap.",
+                    "A book just fell on my head, I only have my shelf to blame.",
+                    "3.14% of sailors are Pi Rates.",
+                    "A moon rock tastes better than an earthly rock because it's meteor."
+                };
             await ReplyAsync(randomMsgs[random.Next(3)]);
         }
     }
